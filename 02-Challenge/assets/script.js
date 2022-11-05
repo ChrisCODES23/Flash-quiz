caf81b0da6a59b65da6f3d76724d4519a32d4d73
ChrisCODES23 <christiantalledo89@gmail.com>
2022-11-04 20:06:52 -0700

    finished code

--- 02-Challenge/assets/script.js
+++ 02-Challenge/assets/script.js
@@ -1,120 +1,138 @@
-var questions = [];
-var hidden = document.getElementById('questions')
-var startButton = document.getElementById('start-btn')
+var question = document.getElementById("question")
+var highScores = JSON.parse(localStorage.getItem('score')) || [];
+var scores = document.getElementById('scores')
+console.log(question)
+var answers = document.getElementById("answers")
+console.log(answers)
+var nextButton = document.getElementById("nxt-button")
+var startButton = document.getElementById("start-btn")
+console.log(startButton)
 var hsButton = document.getElementById('hs-btn')
 var saveButton = document.getElementById('save-btn')
 var timerCount = 40;
 var timerElement = document.getElementById('time')
 var score = 0;
+var currentIndex = 0;
+var highest = document.getElementById('highScores')
+
 // variable used to store the questions and answers
 var questionArray = [
     {
-        question1: "What should appear at the very end of your JavaScript?",
-        possibleAnswers1: ["The < script >", "The END statement", "None of the above"],
-        correctAnswer1: "</script>"
-
+        question: "What should appear at the very end of your JavaScript?",
+        possibleAnswers: ["The < script >", "The END statement", "None of the above", "</script>"],
+        correctAnswer: "</script>",
     },
     {
-        question2: "Which of the following is not a valid JavaScript variable name?",
-        possibleAnswers2: ["_first_and_last_names", "FirstAndLast", "None of the above"],
-        correctAnswer2: ""
+        question: "Which of the following is not a valid JavaScript variable name?",
+        possibleAnswers: ["_first_and_last_names", "FirstAndLast", "None of the above", "firstLast"],
+        correctAnswer: "None of the above",
     },
     {
-        question3: " ____________operator is used to assign a value to a variable?",
-        possibleAnswers3: [
+        question: " ____________operator is used to assign a value to a variable?",
+        possibleAnswers: [
             "*",
             "x",
-            "+"],
-        correctAnswer3: "="
+            "+", "="],
+        correctAnswer: "=",
     },
     {
-        question4: "The _______ method of an Array object adds and/or removes elements from an array.",
-        possibleAnswers4: [
+        question: "The _______ method of an Array object adds and/or removes elements from an array.",
+        possibleAnswers: [
             "Reverse",
             "Shift",
-            "Slice"],
-        correctAnswer4: "Splice"
+            "Slice", "Splice"],
+        correctAnswer: "slice",
     },
     {
-        question5: "Which statement is not true in java language ?",
-        possibleAnswers5: [" A public member of a class can be accessed in all the packages.",
+        question: "Which statement is not true in java language ?",
+        possibleAnswers: [" A public member of a class can be accessed in all the packages.",
             "A private member of a class cannot be accessed from its derived class.",
-            " A protected member of a class can be accessed from its derived class."],
-        correctAnswer5: "A private member of a class cannot be accessed by the methods of the same class."
+            " A protected member of a class can be accessed from its derived class.", "A private member of a class cannot be accessed by the methods of the same class."],
     },
 ]
-// Random question picker
+// Random question and answers picker
 var currentQuestion = "";
-for (var i = 0; i < questionArray.length; i++) {
-    var randomIndex = Math.floor(Math.random() * questionArray.length);
-    questions.textContent = randomIndex
-    randomIndex = currentQuestion;
-}
 
+const lastQuestion = currentQuestion - 1;
 // starts the game
 function startGame() {
     console.log("Started the game yay");
-    startButton.style.visibility = "hidden";
-    hsButton.style.visibility = "hidden";
-    saveButton.style.visibility = "hidden";
-    hidden.style.visibility = "visible";
+    answers.classList.remove('hidden');
+    nextButton.classList.remove('hidden');
+    saveButton.classList.remove('hidden');
+    hsButton.classList.remove('hidden');
+    question.classList.remove('hidden');
+    // now i will set the css to visible display block
     startTimer();
-
+    setNextQuestion();
     //  Gives the next question and answers
-    function setNextQuestion() {
-        if (currentQuestion > 1)
-            questions.textContent = 
+}
+function setNextQuestion() {
+    if (currentIndex < questionArray.length) {
+        question.textContent = questionArray[currentIndex].question;
+        for (var i = 0; i < 4; i++) {
+            answers.children[i].textContent = questionArray[currentIndex].possibleAnswers[i];
+            answers.children[i].addEventListener('click', selectAnswer)
+        }
+        nextButton.addEventListener('click', setNextQuestion)
     }
-
-
-    function selectAnswer() {
-        EventTarget
+    else {
+        endGame();
     }
 }
-
-
 function saveGame() {
     console.log("Saved to the highscores yay");
-    localStorage.setItem
+    highScores.push(score)
+    localStorage.setItem('score', JSON.stringify(highScores))
+    highScore();
 }
 function highScore() {
     console.log(" Did you get the highest score?");
-    localStorage.getItem
+    for (var i = 0; i < highScores.length; i++) {
+        var newLi = document.createElement('li')
+        newLi.textContent = highScores[i];
+        highest.append(newLi)
+    }
+
 }
-
-
-
-// if (currentQuestion > lastQuestion - 1) then display next question
-// if (currentQuestion > lastQuestion - 1)
-
-
-
-
-startButton.addEventListener('click', startGame)
-saveButton.addEventListener('click', saveGame)
-hsButton.addEventListener('click', highScore)
-nxtButton.addEventListener('click', setNextQuestion)
-
-
-
 // Time starts at 40 and incorrect answer will deduct 10 secs from the timercount.
-// ? Doesn't function well
 function startTimer() {
-
     timer = setInterval(function () {
-        timerCount--;
         timerElement.textContent = timerCount;
-        // if (!currentQuestion) {
-        //     timerCount - 10;
-        //     timerElement.textContent = timerCount
-        //     clearInterval(timer);
-
-
+        timerCount--;
     }, 1000);
 }
 
+// Challenge Question (time+10)
+function challenge() {
+    timerCount += 10;
+    timerElement.textContent = timerCount;
+}
+
+startButton.addEventListener('click', startGame)
+saveButton.addEventListener('click', saveGame)
 
 
 
+function selectAnswer(event) {
+    var isCorrect = event.target.textContent == questionArray[currentIndex].correctAnswer
+    if (!isCorrect) {
+        timerCount -= 10;
+    }
+    else {
+        score += 5;
+        scores.textContent = "Score: " + score;
 
-// ? Challenge Question (time+10)
+    }
+    currentIndex++;
+    setNextQuestion();
+
+}
+
+function endGame() {
+    answers.classList.add('hidden');
+    nextButton.classList.add('hidden');
+    hsButton.classList.add('hidden');
+    question.classList.add('hidden');
+    clearInterval(timer)
+}
